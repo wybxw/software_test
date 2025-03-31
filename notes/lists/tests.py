d087e1fee2dc27319085542e364cde63221e29ad
wybxw <3343156983@qq.com>
2025-03-31 10:58:40 +0800

    message 4:First unit test

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -12,7 +12,10 @@
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
 from django.test import TestCase
-class smoketest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+from django.urls import resolve
+from lists.views import home_page
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
 
---
+++ notes/lists/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+home_page = None
+# Create your views here.
