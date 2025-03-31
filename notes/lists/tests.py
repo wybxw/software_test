ae59266a8b356493cfb0436b69df80a161b9e833
wybxw <3343156983@qq.com>
2025-03-31 11:09:54 +0800

    message 5 : The first iterative developmen

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -1,21 +1,17 @@
-# from selenium import webdriver
-# import unittest
-# class NewVisitorTest(unittest.TestCase):
-#     def setUp(self):
-#         self.browser = webdriver.Chrome()
-#     def tearDown(self):
-#         self.browser.quit()
-#     def test_can_start_a_list_and_retrieve_it_later(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('install', self.browser.title) , "Browser title was " + self.browser.title
-#         self.fail('Finish the test!')
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
+
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
-
+    def test_home_page_return_correct_html(self):
+        request =HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
+        
--- notes/lists/views.py
+++ notes/lists/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-home_page = None
+from django.http import HttpResponse
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 # Create your views here.
