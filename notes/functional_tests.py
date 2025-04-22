ddd326ff0d313aa7af3e07584946c26b5a23e12a
吴 优 <3343156983@qq.com>
2025-04-23 00:02:26 +0800

    message 7: 3.1

--- notes/functional_tests.py
+++ notes/functional_tests.py
@@ -14,12 +14,14 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do',header_text)
         inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys('Buy flowers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID,'id_list_table')
         rows=table.find_elements(By.TAG_NAME,'tr')
-        self.assertin('1:Buy flowers',[row.text for row in rows])
+        self.assertIn('1:Buy flowers',[row.text for row in rows])
 
         
         self.fail('Finish the test!')
