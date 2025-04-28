5e555dca6ea59d10cfe583f747ee58ae0f3afcf3
吴 优 <3343156983@qq.com>
2025-04-28 10:36:47 +0800

    message 9: After refactoring the functional_tests

--- notes/functional_tests.py
+++ notes/functional_tests.py
@@ -8,6 +8,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title) , "Browser title was " + self.browser.title
@@ -29,6 +33,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('1:Buy flowers',[row.text for row in rows])
         self.assertIn('2:Give a gift to Lisi', [row.text for row in rows])
         
+        self.check_for_row_in_list_table('1:Buy flowers')
+        self.check_for_row_in_list_table('2:Give a gift to Lisi')
+
         self.fail('Finish the test!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
