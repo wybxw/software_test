ce7014af3b5d8086035bbf27c7e7fcf4cd163c52
吴 优 <3343156983@qq.com>
2025-04-28 11:21:55 +0800

    message 14: add a new user

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -82,5 +82,37 @@ class NewVisitorTest(LiveServerTestCase):
         # self.check_for_row_in_list_table('2:Give a gift to Lisi')
 
         self.fail('Finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy flowers')
+
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url,'/lists/.+')
+        # self.wait_for_row_in_list_table('1:Buy a gift for Lisi')
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertNotIn('Give a gift to Lisi',page_text)
+
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy milk')
+
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url,'/lists/.+')
+        self.assertEqual(wangwu_list_url,zhangsan_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertIn('Buy milk',page_text)
+
+        
 # if __name__ == '__main__':
     # unittest.main(warnings='ignore')
