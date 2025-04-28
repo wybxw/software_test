eac61a17721d9409963a9b60925352aab3a9072e
吴 优 <3343156983@qq.com>
2025-04-28 10:10:50 +0800

    message 7: Refactoring (重构)

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -3,15 +3,19 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
     def test_home_page_return_correct_html(self):
-        request =HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
-        
+        #v1
+        # request =HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf8')
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>',html)
+        # self.assertTrue(html.endswith('</html>'))
+        #v2
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
