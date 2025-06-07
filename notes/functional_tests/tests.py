445a9a1bdbd1851d0ec3bf403e62d5c0bb713206
吴 优 <3343156983@qq.com>
2025-06-08 01:22:30 +0800

    message20: testing for layout + styling

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -121,6 +121,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy flowers',page_text)
         self.assertIn('Buy milk',page_text)
 
-        
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024.768)
+
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta=10
+        )
 # if __name__ == '__main__':
     # unittest.main(warnings='ignore')
