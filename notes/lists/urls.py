c1949c42847ccb8bce5a3ff4ac43653e35756ed8
吴 优 <3343156983@qq.com>
2025-06-08 01:12:59 +0800

    message19: finish 14 chapter

--- notes/lists/urls.py
+++ notes/lists/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path,re_path,include
-from lists import views as lists_views
-from lists import urls as lists_urls
+from django.contrib import admin
+from django.urls import path,re_path
+from lists import views
 urlpatterns = [
-    path('', lists_views.home_page, name='home'),
-    path('lists/' , include(lists_urls))
+    # path("admin/", admin.site.urls),
+    # path('',views.home_page,name='home'),
+    path('new',views.new_list,name='new_list'),
+    # path('lists/the-new-page/',views.view_list,name='view_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
